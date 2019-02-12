7224e0f5dfd0332b12ee2b9b28ff64369810401b
Carter Capocaccia <ccapocaccia@uaminc.com>
2019-02-12 14:42:11 -0600

    adds how it works to tagline

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -7,6 +7,13 @@
       <h2 class="title">
         Nobody plans to get lost. Leave a Blip. Be found.
       </h2>
+      <h4>
+        How it works:
+      </h4>
+      <h5>
+        Add your contacts.  Add a Blip. When your Blip ends, we will check on you with an email.
+        If the email isnt acknowledged, we notify your contacts with the notes you provided.
+      </h5>
     </div>
     <div class="login-wrapper columns is-centered">
       <login>
