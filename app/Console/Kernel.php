f2a9d0ace87031a98241b7cc9721f832984a80f8
Carter Capocaccia <ccapocaccia@uaminc.com>
2019-02-21 09:14:38 -0600

    merges in testing changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('emails:troll')->everyFiveMinutes();
+        $schedule->command('emails:troll')->dailyAt('12:00');
     }
 
     /**
