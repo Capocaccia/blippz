cc8c0a56befcfe5f753f0836cc2027f361253661
Carter Capocaccia <carter.capocaccia@gmail.com>
2019-07-10 20:11:36 -0500

    moves troll to run every fifteen minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('emails:troll')->dailyAt('12:00');
+        $schedule->command('emails:troll')->everyFiveMinutes();
     }
 
     /**
